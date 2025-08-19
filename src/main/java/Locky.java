f76d10bb1c0529d155a34039d25762f1efe1392c
Emilia Ayu <emiliaayu944@gmail.com>
2025-08-20 06:13:02 +0800

    The chatbot only greets the user once and then waits for input, but does not properly echo commands or exit cleanly when "bye" is typed.
    Without a loop that echoes the commands, the chatbot will not meet Level 1 requirements
    
    An input loop that echoes any user command back to the console is implemented, and "bye" is added as a termination connection.
    
    Echoing commands is the most rudimentary way to assess the chatbot's interaction with user response, and adding a termination condition ensures it does not run forever.

--- src/main/java/Locky.java
+++ src/main/java/Locky.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Locky {
     public static void main(String[] args) {
         String logo = "     __________ \n" +
@@ -14,10 +17,19 @@ public class Locky {
         String intro = "____________________________________________________________\n"
         + "Hello! I'm Locky\n"
         + "Reminding you to Lock In!\n"
-        + "What can I do for you?\n"
-        + "____________________________________________________________\n"
-        + "You better Lock In!\n"
-        + "____________________________________________________________";
+        + "What can I do for you?\n";
         System.out.println(logo + intro);
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (!Objects.equals(command, "bye")) {
+            System.out.println("____________________________________________________________\n"
+                    + command
+                    + "\n");
+            command = scanner.nextLine();
+        }
+        System.out.println("____________________________________________________________\n"
+                + "You better Lock In!\n"
+                + "____________________________________________________________");
+        scanner.close();
     }
 }
