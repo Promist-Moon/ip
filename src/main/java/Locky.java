463df8b484c137480464c50c1073eed10fe7392f
Emilia Ayu <emiliaayu944@gmail.com>
2025-08-20 14:02:40 +0800

    The chatbot currently has no way to retain previous commands and list them out.
    The chatbot should be able to store commands in a list and return them as prompted, in order to meet Duke Level-2 requirements.
    
    Implement an ArrayList to store user-entered text, update the loop to add non-list commands to the list, and handle list command to print all stored tasks.
    
    ArrayList has no fixed limit on the number of items in a list, hence can expand dynamically. Additional conditional should be implemented to add list feature.
    
    Each added task is acknowledged with a confirmation message, and tasks are displayed in a numbered format when "list" is entered.

--- src/main/java/Locky.java
+++ src/main/java/Locky.java
@@ -1,8 +1,10 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Locky {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________\n";
         String logo = "     __________ \n" +
                 "    / .------. \\\n" +
                 "   / /        \\ \\\n" +
@@ -14,22 +16,35 @@ public class Locky {
                 "'       LOCKY      '\n" +
                 "|   '. .    . .'   |\n" +
                 "'.________________.'\n";
-        String intro = "____________________________________________________________\n"
+        String intro = divider
         + "Hello! I'm Locky\n"
         + "Reminding you to Lock In!\n"
         + "What can I do for you?\n";
+
+        ArrayList<String> list = new ArrayList<>();
+
         System.out.println(logo + intro);
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
         while (!Objects.equals(command, "bye")) {
-            System.out.println("____________________________________________________________\n"
+            System.out.println(divider);
+            if (Objects.equals(command, "list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+                System.out.println("\n");
+                command = scanner.nextLine();
+                continue;
+            }
+            System.out.println("added: "
                     + command
                     + "\n");
+            list.add(command);
             command = scanner.nextLine();
         }
-        System.out.println("____________________________________________________________\n"
+        System.out.println(divider
                 + "You better Lock In!\n"
-                + "____________________________________________________________");
+                + divider);
         scanner.close();
     }
 }
