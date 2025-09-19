8625d65778e4daae374f3749596d5ba7a84bb567
Emilia Ayu <emiliaayu944@gmail.com>
2025-09-19 14:35:19 +0800

    Add message when executing ListCommand

--- src/main/java/locky/commands/ListCommand.java
+++ src/main/java/locky/commands/ListCommand.java
@@ -10,6 +10,12 @@ import locky.tasks.TaskList;
 public class ListCommand implements Command {
     @Override
     public String execute(TaskList list) {
-        return list.getListString();
+        String message;
+        if (list.isEmpty()) {
+            message = "Looky looky your Locky task list is empty! Time to get started!\n";
+        } else {
+            message = "Oh my, look at all these tasks! Chop chop!\n";
+        }
+        return message + list.getListString();
     }
 }
